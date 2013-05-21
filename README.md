6566852e47890a0161450ca9e8bc64a018d42f33
outrunthewolf <chris@outrunthewolf.com>
2013-05-22 00:00:50 +0100

    basic js styling incorporated

--- README.md
+++ README.md
@@ -18,3 +18,43 @@ To initiate the script
 		new SuperRainbowCanvas();
 	});
 ```
+
+Basic options are as follows
+```javascript
+    options: 
+    {
+    	container: '', // You can force the canvas to be contained within a particular element
+		size: 
+		{
+    		min: 1, // Min and Max slider sizes
+    		max: 101
+		},
+		styles: // Override styles for the various elements
+		{
+			box: 
+			{
+				'float': 'left',
+				background: '#333'
+			},
+			subControl: 
+			{
+				background: '#dedede',
+				padding: '10px'
+			},
+			slider: 
+			{
+				background: '#333',
+				width: '200px',
+				margin: '2px',
+				padding: '2px;'
+			},
+			knob: 
+			{
+				width: '20px',
+				height: '20px',
+				background: '#ff9600',
+				cursor: 'pointer'
+			}
+		}
+    }
+```
--- src/SuperRainbowCanvas.js
+++ src/SuperRainbowCanvas.js
@@ -6,10 +6,38 @@ var SuperRainbowCanvas = new Class
     {
     	container: '',
     	settings: {},
-    	controlSize: {
+		size: 
+		{
     		min: 1,
     		max: 101
-    	}
+		},
+		styles:
+		{
+			box: 
+			{
+				'float': 'left',
+				background: '#333'
+			},
+			subControl: 
+			{
+				background: '#dedede',
+				padding: '10px'
+			},
+			slider: 
+			{
+				background: '#333',
+				width: '200px',
+				margin: '2px',
+				padding: '2px;'
+			},
+			knob: 
+			{
+				width: '20px',
+				height: '20px',
+				background: '#ff9600',
+				cursor: 'pointer'
+			}
+		}
     },
 
     canvas: {},
@@ -22,14 +50,16 @@ var SuperRainbowCanvas = new Class
 		if(!this.options.container)
 			this.options.container = document.body;
 
-        this.setControlRange(this.options.controlSize);
+        this.setControlRange(this.options);
         this.buildCanvas(this.options.container);
         this.buildControls(this.options);
+
+        
     },
 
     buildCanvas: function(container)
     {
-		canvas = new Element('canvas', {
+		this.canvas = new Element('canvas', {
 			id: 'SuperRainbowCanvas',
 			styles: {
 				height: '100%',
@@ -42,49 +72,62 @@ var SuperRainbowCanvas = new Class
 		}).inject(container);
     },
 
-    setControlRange: function(controlSize)
+    setControlRange: function(controls)
     {
     	this.controls = 
     	{
 	    	mainControls: 
 			{ 
-				size: { range: { min: controlSize.min, max: controlSize.max }}, 
-				phase: { range: { min: controlSize.min, max: controlSize.max }}
+				size: { range: { min: controls.size.min, max: 21 }}, 
+				phase: { range: { min: controls.size.min, max: controls.size.max }}
 			}, 
 			phaseControls: 
 			{ 
-				red: {range: { min: controlSize.min, max: controlSize.max }}, 
-				green: {range: { min: controlSize.min, max: controlSize.max }}, 
-				blue: {range: { min: controlSize.min, max: controlSize.max }}
+				red: {range: { min: controls.size.min, max: controls.size.max }}, 
+				green: {range: { min: controls.size.min, max: controls.size.max }}, 
+				blue: {range: { min: controls.size.min, max: controls.size.max }}
 			},
 			freqControls: 
 			{ 
-				'red-frequency': {range: { min: controlSize.min, max: controlSize.max }}, 
-				'green-frequency': {range: { min: controlSize.min, max: controlSize.max }}, 
-				'blue-frequency': {range: { min: controlSize.min, max: controlSize.max }}
+				'red-frequency': {range: { min: controls.size.min, max: controls.size.max }}, 
+				'green-frequency': {range: { min: controls.size.min, max: controls.size.max }}, 
+				'blue-frequency': {range: { min: controls.size.min, max: controls.size.max }}
 			}
 		}
     },
 
     buildControls: function(options)
     {
-
+		var self = this;
 		var wrapper = new Element('section#wrapper').inject(options.container);
-		var control = new Element('div#control').inject(wrapper);
+		var control = new Element('div#control', { styles: options.styles.box }).inject(wrapper);
 		var i = 0;
+		
 
     	// loop through main controls and apppend each child to stage
 		Object.each(this.controls, function(v, k) 
 		{		
 			// create sub-control element
-			var subC = new Element('div', { 'class': 'sub-control', html: '<h2>' + k + '</h2>' });
+			var subC = new Element('div', 
+			{ 
+				'class': 'sub-control', 
+				html: '<h2 style="margin: 0px 0px 10px 0px">' + k.capitalize() + '</h2>',
+				styles: options.styles.subControl
+			});
 			$('control').adopt(subC);
 			
 			// Adopt All sub-control sub elements
 		    Object.each(v, function(v, k) {
-				var el = new Element('div', { id: k, 'class': 'slider', html: '<div class="knob"></div><span>' + k + '</span><div class="text"></div>' });
+				var el = new Element('div', 
+				{ 
+					id: k, 
+					'class': 'slider', 
+					html: '<div class="knob"></div><span style="color: #fff">' + k + '</span><div class="text" style="color: #fff;"></div>', 
+					styles: options.styles.slider
+				});
 				$$('.sub-control')[i].adopt(el);
 			});
+			$$('.knob').setStyles(options.styles.knob);
 			++i;
 		});
 
@@ -98,12 +141,41 @@ var SuperRainbowCanvas = new Class
 					onChange: function() {
 						options.settings[k] = this.step;
 						a.getElement('.text').set('html', (this.step));
-						//scene.loadScene();
+						self.loadScene();
 					}
 				});
 			});
-		});	   
+		});	
+    },
 
-    }
+	loadScene: function() 
+	{
+		var context = this.canvas.getContext("2d");
+		var size = this.canvas.getSize();
+		var amount = size.y / this.options.settings.size;
+		this.options.settings.centre = 128;
+		
+		for(var i = 0; i < amount;) {
+			
+			red   = Math.sin((this.options.settings['red-frequency'] / 100) * i + (this.options.settings.red / 10) + (this.options.settings.phase / 10)) * 127 + this.options.settings.centre;
+			green = Math.sin((this.options.settings['green-frequency'] / 100) * i + (this.options.settings.green / 10)  + (this.options.settings.phase / 10)) * 127 + this.options.settings.centre;
+			blue  = Math.sin((this.options.settings['blue-frequency'] / 100)  * i + (this.options.settings.blue / 10)  + (this.options.settings.phase / 10)) * 127 + this.options.settings.centre;
+			context.fillStyle = this.RGB2Color(red, green, blue);
+			context.fillRect (0, i * this.options.settings.size, size.x, this.options.settings.size);
+			++i;
+		}
+	},
+
+	// Mootools functions for this aren't very good for some reason, so these are custom
+	RGB2Color: function(r,g,b)
+	{
+		return '#' + this.byte2Hex(r) + this.byte2Hex(g) + this.byte2Hex(b);
+	},
+	
+	byte2Hex: function(n)
+	{
+		var nybHexString = "0123456789ABCDEF";
+		return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
+	}
 
 });
